9bb21b3c90fdd00791c7d18e58cb0cefe0329e1b
unknown <ntienlong69@gmail.com>
2025-06-24 22:10:31 +0700

    sua lai login

--- docs/login/login.js
+++ docs/login/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     
     if (username === 'tienlongCVA' && password === 'long10pen') {
-        window.location.href = '../pagechinh/index.html';
+        window.location.href = '/docs/index.html';
     } else {
         document.getElementById('errorMessage').innerText = 'Invalid username or password';
     }
