94a0fb9a12c9f13739b049e7e8700a076ba762cd
unknown <ntienlong69@gmail.com>
2025-06-25 14:09:56 +0700

    sua lai co che login

--- docs/login/login.js
+++ docs/login/login.js
@@ -1,11 +1,13 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    if (username === 'tienlongCVA' && password === 'long10pen') {
-        window.location.href = '/docs/index.html';
-    } else {
-        document.getElementById('errorMessage').innerText = 'Invalid username or password';
-    }
+  event.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'tienlongCVA' && password === 'long10pen') {
+    localStorage.setItem('loggedIn', 'true');
+    window.location.href = "/docs/index.html";
+  } else {
+    document.getElementById('errorMessage').innerText = "Sai thông tin đăng nhập!";
+  }
 });
+    
