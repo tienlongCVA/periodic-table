b1410eddcc04ea93f27a6241b0fbd60fac2e39ec
unknown <ntienlong69@gmail.com>
2025-10-24 20:54:27 +0700

    lam tron kl nguyen tu

--- docs/elements.js
+++ docs/elements.js
@@ -1,11 +1,11 @@
 const elements = {
-  H:  { name: "Hydrogen", symbol: "H", number: 1,  mass: 1.008 },
-  He: { name: "Helium",   symbol: "He", number: 2,  mass: 4.0026 },
-  Li: { name: "Lithium",  symbol: "Li", number: 3,  mass: 6.94 },
-  Be: { name: "Beryllium",symbol: "Be", number: 4,  mass: 9.0122 },
-  B:  { name: "Boron",    symbol: "B", number: 5,  mass: 10.81 },
-  C:  { name: "Carbon",   symbol: "C", number: 6,  mass: 12.011 },
-  N:  { name: "Nitrogen", symbol: "N", number: 7,  mass: 14.007 },
-  O:  { name: "Oxygen",   symbol: "O", number: 8,  mass: 15.999 },
-  Ne: { name: "Neon",     symbol: "Ne", number: 10, mass: 20.180 }
+  H:  { name: "Hydrogen", symbol: "H", number: 1,  mass: 1 },
+  He: { name: "Helium",   symbol: "He", number: 2,  mass: 4 },
+  Li: { name: "Lithium",  symbol: "Li", number: 3,  mass: 6 },
+  Be: { name: "Beryllium",symbol: "Be", number: 4,  mass: 9 },
+  B:  { name: "Boron",    symbol: "B", number: 5,  mass: 11 },
+  C:  { name: "Carbon",   symbol: "C", number: 6,  mass: 12 },
+  N:  { name: "Nitrogen", symbol: "N", number: 7,  mass: 14 },
+  O:  { name: "Oxygen",   symbol: "O", number: 8,  mass: 16 },
+  Ne: { name: "Neon",     symbol: "Ne", number: 10, mass: 20 }
 };
