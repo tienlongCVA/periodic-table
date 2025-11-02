147446692e25cb5fcb28af6b7c48460650b0ae15
unknown <ntienlong69@gmail.com>
2025-11-03 00:07:20 +0700

    them video va them chuc nang trong muc video

--- docs/video/video.js
+++ docs/video/video.js
@@ -1,5 +1,26 @@
-
-// Đăng xuất
 document.getElementById("Home").addEventListener("click", () => {
   window.location.href = "../index.html";
+});
+
+// Chức năng lọc video
+const btnAll = document.getElementById("btnAll");
+const btnVN = document.getElementById("btnVN");
+const btnEN = document.getElementById("btnEN");
+
+const vnVideos = document.querySelectorAll(".video-card.vn");
+const enVideos = document.querySelectorAll(".video-card.en");
+
+btnAll.addEventListener("click", () => {
+  vnVideos.forEach(v => v.style.display = "block");
+  enVideos.forEach(v => v.style.display = "block");
+});
+
+btnVN.addEventListener("click", () => {
+  vnVideos.forEach(v => v.style.display = "block");
+  enVideos.forEach(v => v.style.display = "none");
+});
+
+btnEN.addEventListener("click", () => {
+  vnVideos.forEach(v => v.style.display = "none");
+  enVideos.forEach(v => v.style.display = "block");
 });
